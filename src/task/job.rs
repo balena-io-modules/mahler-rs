94eec98153faf817a5b1c8a75255c7916487ff52
Felipe Lalanne <felipe@balena.io>
2024-09-13 19:21:51 -0300

    Add Id and description to Job

--- src/task/job.rs
+++ src/task/job.rs
@@ -2,11 +2,25 @@ use super::boxed::*;
 use super::{Handler, Method, Task};
 use crate::system::Context;
 
+fn get_id_and_name<T>() -> (String, String) {
+    let id = std::any::type_name::<T>();
+
+    // Find and cut the rest of the path
+    let name = match &id[..id.len() - 3].rfind(':') {
+        Some(pos) => &id[pos + 1..id.len()],
+        None => &id[..id.len()],
+    };
+
+    (id.to_string(), name.to_string())
+}
+
 /// Jobs are generic work definitions. They can be converted to tasks
 /// by calling into_task with a specific context.
 ///
 /// Jobs are re-usable
 pub struct Job<S> {
+    id: String,
+    description: Box<dyn Fn(Context<S>) -> String>,
     builder: BoxedIntoTask<S>,
 }
 
@@ -17,7 +31,17 @@ impl<S> Job<S> {
         S: 'static,
         I: 'static,
     {
+        let (id, name) = get_id_and_name::<H>();
+
         Self {
+            id,
+            description: Box::new(move |context| {
+                let path = context.path;
+                if path.to_str().is_empty() {
+                    return name.clone();
+                }
+                format!("{}: {}", name, path)
+            }),
             builder: BoxedIntoTask::from_handler(handler),
         }
     }
@@ -27,9 +51,26 @@ impl<S> Job<S> {
         M: Method<S, T>,
         S: 'static,
     {
+        let (id, name) = get_id_and_name::<M>();
         Self {
+            id,
+            description: Box::new(move |context| {
+                let path = context.path;
+                if path.to_str().is_empty() {
+                    return name.clone();
+                }
+                format!("{}: {}", name, path)
+            }),
             builder: BoxedIntoTask::from_method(method),
         }
+    }
+
+    pub fn id(&self) -> String {
+        self.id.clone()
+    }
+
+    pub fn description(&self, context: Context<S>) -> String {
+        (self.description)(context)
     }
 
     pub fn into_task(&self, context: Context<S>) -> Task<S> {
