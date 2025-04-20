8fd9a6c7092d247cb7d7dbe8cb0b7f31eec27793
Felipe Lalanne <felipe@balena.io>
2025-04-20 19:56:28 -0400

    Fix run-tests script
    Updates the script to exit on the first test failure
    
    Change-type: patch

--- examples/run-tests.sh
+++ examples/run-tests.sh
@@ -5,10 +5,12 @@ set -e
 RUST_LOG="${RUST_LOG:-debug}"
 export RUST_LOG="${RUST_LOG},bollard=off,hyper=off"
 
-# Run the tests inside every directory with
-# a Cargo.tom
-find ./examples -name 'Cargo.toml' -exec sh -c '
-    dir=$(dirname "$1")
+# Run the tests inside every directory with a Cargo.toml
+find ./examples -name 'Cargo.toml' | while read -r file; do
+    dir=$(dirname "$file")
     echo "Running tests in $dir"
-    (cd "$dir" && cargo test -- --test-threads 1)
-  ' sh {} \;
+    (cd "$dir" && cargo test -- --test-threads 1) || {
+        echo "Tests failed in $dir"
+        exit 1
+    }
+done
